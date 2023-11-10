3d3574ade20b64e6fb440e6118d65280f75dd70c
Kevin Paiz Ramos <140773589+KevinPaizRamos@users.noreply.github.com>
2023-11-10 16:21:23 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+public class Main {
+    public static void main(String[]args){
+
+
+    window f1 = new window();
+    f1.setTitle("Snake");
+    f1.setSize(300,300);
+    f1.setVisible(true);
+    f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+}
